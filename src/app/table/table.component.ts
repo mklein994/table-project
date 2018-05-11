4004077010977218a32c05eb97bc2d2f970aed55
Matthew Klein <matthewklein@winnipeg.ca>
2018-05-11 15:57:11 -0500

    Reformat settings object for table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -19,76 +19,29 @@ export class TableComponent implements OnInit {
   constructor(private backendService: BackendService) {
     this.config = {
       settings: [
-        {
-          // number (eg. 1)
-          objectKey: 'summerServiceId',
-        }, {
-          // Date (eg. "2018-02-21T00:00:00")
-          objectKey: 'installDate',
-        }, {
-          // number (eg. null)
-          objectKey: 'installedById',
-        }, {
-          // string (eg. "")
-          objectKey: 'installedByName',
-        }, {
-          // Date (eg. null)
-          objectKey: 'removedDate',
-        }, {
-          // number (eg. null)
-          objectKey: 'removedById',
-        }, {
-          // string (eg. "")
-          objectKey: 'removedByName',
-        }, {
-          // string (eg. "W-HY70025368")
-          objectKey: 'hydrantAssetNumber',
-        }, {
-          // string (eg. "8675309")
-          objectKey: 'meterNumber',
-        }, {
-          // string (eg. "SIZE8")
-          objectKey: 'meterSizeCode',
-        }, {
-          // string (eg. null)
-          objectKey: 'meterSizeDescription',
-        }, {
-          // string (eg. "123456.7")
-          objectKey: 'startMeterReading',
-        }, {
-          // string (eg. "123456.8")
-          objectKey: 'endMeterReading',
-        }, {
-          // string (eg. null)
-          objectKey: 'location',
-        }, {
-          // string (eg. "987654321")
-          objectKey: 'serviceRequestNumber',
-        }, {
-          // string (eg. "Joe Smith")
-          objectKey: 'contact',
-        }, {
-          // string (eg. "204-555-1234")
-          objectKey: 'telephoneNumber',
-        }, {
-          // string (eg. "yaqoHrm{oQ")
-          objectKey: 'geography',
-        }, {
-          // string (eg. null)
-          objectKey: 'modifiedBy',
-        }, {
-          // Date (eg. "0001-01-01T00:00:00")
-          objectKey: 'modifiedDate',
-        }, {
-          // string (eg. null)
-          objectKey: 'modifiedGeography',
-        }, {
-          // number (eg. 0)
-          objectKey: 'version',
-        }, {
-          // Date (eg. "2018-01-14T00:00:00")
-          objectKey: 'createdDate',
-        },
+        { objectKey: 'summerServiceId' }, // number (eg. 1)
+        { objectKey: 'installDate' }, // Date (eg. "2018-02-21T00:00:00")
+        { objectKey: 'installedById' }, // number (eg. null)
+        { objectKey: 'installedByName' }, // string (eg. "")
+        { objectKey: 'removedDate' }, // Date (eg. null)
+        { objectKey: 'removedById' }, // number (eg. null)
+        { objectKey: 'removedByName' }, // string (eg. "")
+        { objectKey: 'hydrantAssetNumber' }, // string (eg. "W-HY70025368")
+        { objectKey: 'meterNumber' }, // string (eg. "8675309")
+        { objectKey: 'meterSizeCode' }, // string (eg. "SIZE8")
+        { objectKey: 'meterSizeDescription' }, // string (eg. null)
+        { objectKey: 'startMeterReading' }, // string (eg. "123456.7")
+        { objectKey: 'endMeterReading' }, // string (eg. "123456.8")
+        { objectKey: 'location' }, // string (eg. null)
+        { objectKey: 'serviceRequestNumber' }, // string (eg. "987654321")
+        { objectKey: 'contact' }, // string (eg. "Joe Smith")
+        { objectKey: 'telephoneNumber' }, // string (eg. "204-555-1234")
+        { objectKey: 'geography' }, // string (eg. "yaqoHrm{oQ")
+        { objectKey: 'modifiedBy' }, // string (eg. null)
+        { objectKey: 'modifiedDate' }, // Date (eg. "0001-01-01T00:00:00")
+        { objectKey: 'modifiedGeography' }, // string (eg. null)
+        { objectKey: 'version' }, // number (eg. 0)
+        { objectKey: 'createdDate' }, // Date (eg. "2018-01-14T00:00:00")
       ],
       fields: [
         {
